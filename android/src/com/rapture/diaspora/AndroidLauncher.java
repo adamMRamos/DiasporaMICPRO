ef69fdc550ff043b9ab991c546a28308db72f5e0
adamMRamos <amram@DIASPORA-MICPRO>
2016-03-09 18:58:51 -0500

    hmmm
    mmmh

---
+++ android/src/com/rapture/diaspora/AndroidLauncher.java
@@ -0,0 +1,19 @@
+package com.rapture.diaspora;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.rapture.diaspora.RaptureDiasporaGame;
+
+public class AndroidLauncher extends AndroidApplication 
+{
+	@Override
+	protected void onCreate (Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RaptureDiasporaGame(), config);
+	}
+}
---
+++ html/src/com/rapture/diaspora/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.rapture.diaspora.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.rapture.diaspora.RaptureDiasporaGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new RaptureDiasporaGame();
+        }
+}
